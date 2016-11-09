8a04e37ba73a6a8206b643c2184622ab12557422
Brianna Nusbaum <bnusbaum.sh@mysummitps.org>
2016-11-09 22:20:37 +0000

    update

--- todo.js
+++ todo.js
@@ -7,12 +7,25 @@ var showTasks = document.getElementById("showTasks");
 
 //Activates the button to make it appear in div and changes the color of the text that appears on the screen when selected in the dropdown\\
 submit.addEventListener("click",function(){
-    var change="<div style= 'color:" + selectColor.value + ";'>"+makeTasks.value +"</div>"
+    var change="<div style= 'color:" + selectColor.value + ";'>"+makeTasks.value +"</div>";
     showTasks.innerHTML= showTasks.innerHTML+change;
    });
 
-//able to delete the items from the list with a click\\
+//able to cross out items with one click\\
+
 showTasks.addEventListener("click",function(evt){
+//   var cross="<div style='text-decoration:" + line-through + ";'></div>"
+//   showTasks.innerHTML=showTasks.innerHTML+cross
+
+    evt.target.style.textDecoration="line-through";
+    
+    
+    
+    
+});
+
+//able to delete items from list with double click\\
+showTasks.addEventListener("dblclick",function(evt){
     var remove= evt.target;
     remove.parentNode.removeChild(remove);
 });
